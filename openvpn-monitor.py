e54b5841e2b1047ea59f44afdfa1baaf0708be6f
Marcus Furlong <furlongm@gmail.com>
2016-02-24 20:29:45 +1100

    add jquery dependency

--- openvpn-monitor.py
+++ openvpn-monitor.py
@@ -459,6 +459,7 @@ def html_header(settings, vpns, maps):
     if maps:
         google_maps_js(vpns, loc_lat, loc_long)
 
+    print '<script src="//code.jquery.com/jquery-1.12.1.min.js"></script>'
     print '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">'
     print '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">'
     print '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>'
