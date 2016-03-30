ce4fa696926ed372a4c1e5e6eaeaec4b0837866e
Marcus Furlong <furlongm@gmail.com>
2016-03-30 22:47:51 +1100

    convert openvpn functions to use class

--- openvpn-monitor.py
+++ openvpn-monitor.py
@@ -45,20 +45,6 @@ def debug(*objs):
     print("DEBUG:\n", *objs, file=sys.stderr)
 
 
-def socket_send(s, command):
-    if sys.version_info[0] == 2:
-        s.send(command)
-    else:
-        s.send(bytes(command, 'utf-8'))
-
-
-def socket_recv(s, length):
-    if sys.version_info[0] == 2:
-        return s.recv(length)
-    else:
-        return s.recv(length).decode('utf-8')
-
-
 def get_date(date_string, uts=False):
     if not uts:
         return datetime.strptime(date_string, "%a %b %d %H:%M:%S %Y")
@@ -123,229 +109,243 @@ class config_loader(object):
             debug("=== begin section\n{0!s}\n=== end section".format(vpn))
 
 
-def openvpn_connect(vpn):
+class openvpn_monitor(object):
 
-    host = vpn['host']
-    port = int(vpn['port'])
-    timeout = 3
+    def __init__(self, vpns):
+        self.vpns = vpns
+        for key, vpn in list(self.vpns.items()):
+            s = self.connect(vpn)
+            if s:
+                self.disconnect(s)
+                self.collect_data(vpn)
 
-    try:
-        s = socket.create_connection((host, port), timeout)
-        vpn['socket_connected'] = True
-        return s
-    except socket.error:
-        vpn['socket_connected'] = False
-        return False
+    def _socket_send(self, s, command):
+        if sys.version_info[0] == 2:
+            s.send(command)
+        else:
+            s.send(bytes(command, 'utf-8'))
 
+    def _socket_recv(self, s, length):
+        if sys.version_info[0] == 2:
+            return s.recv(length)
+        else:
+            return s.recv(length).decode('utf-8')
 
-def openvpn_disconnect(s):
-    s.close()
+    def collect_data(self, vpn):
+        version = self.send_command(vpn, 'version\n')
+        vpn['version'] = self.parse_version(version)
+        state = self.send_command(vpn, 'state\n')
+        vpn['state'] = self.parse_state(state)
+        stats = self.send_command(vpn, 'load-stats\n')
+        vpn['stats'] = self.parse_stats(stats)
+        status = self.send_command(vpn, 'status 3\n')
+        vpn['sessions'] = self.parse_status(status)
 
+    def connect(self, vpn):
+        host = vpn['host']
+        port = int(vpn['port'])
+        timeout = 3
+        try:
+            s = socket.create_connection((host, port), timeout)
+            vpn['socket_connected'] = True
+            return s
+        except socket.error:
+            vpn['socket_connected'] = False
+            return False
 
-def openvpn_send_command(vpn, command):
+    def disconnect(self, s):
+        s.close()
 
-    s = openvpn_connect(vpn)
-    data = ''
-    socket_send(s, command)
-
-    while 1:
-        socket_data = socket_recv(s, 1024)
-        socket_data = re.sub('>INFO(.)*\r\n', '', socket_data)
-        data += socket_data
-        if command == 'load-stats\n' and data != '':
-            break
-        elif data.endswith("\nEND\r\n"):
-            break
-
-    socket_send(s, 'quit\n')
-    openvpn_disconnect(s)
-
-    if args.debug:
-        debug("=== begin raw data\n{0!s}\n=== end raw data".format(data))
-
-    return data
-
-
-def openvpn_parse_state(data):
-
-    state = {}
-
-    for line in data.splitlines():
-        tmp = line.split(',')
+    def send_command(self, vpn, command):
+        s = self.connect(vpn)
+        data = ''
+        self._socket_send(s, command)
+        while 1:
+            socket_data = self._socket_recv(s, 1024)
+            socket_data = re.sub('>INFO(.)*\r\n', '', socket_data)
+            data += socket_data
+            if command == 'load-stats\n' and data != '':
+                break
+            elif data.endswith("\nEND\r\n"):
+                break
+        self._socket_send(s, 'quit\n')
+        self.disconnect(s)
         if args.debug:
-            debug("=== begin split line\n{0!s}\n=== end split line".format(tmp))
-        if tmp[0].startswith('>INFO') or \
-           tmp[0].startswith('END') or \
-           tmp[0].startswith('>CLIENT'):
-            continue
-        else:
-            state['up_since'] = get_date(date_string=tmp[0], uts=True)
-            state['connected'] = tmp[1]
-            state['success'] = tmp[2]
-            if tmp[3]:
-                state['local_ip'] = ip_address(tmp[3])
+            debug("=== begin raw data\n{0!s}\n=== end raw data".format(data))
+        return data
+
+    def parse_state(self, data):
+
+        state = {}
+        for line in data.splitlines():
+            parts = line.split(',')
+            if args.debug:
+                debug("=== begin split line\n{0!s}\n=== end split line".format(parts))
+            if parts[0].startswith('>INFO') or \
+               parts[0].startswith('END') or \
+               parts[0].startswith('>CLIENT'):
+                continue
             else:
-                state['local_ip'] = ''
-            if tmp[4]:
-                state['remote_ip'] = ip_address(tmp[4])
-                state['mode'] = 'Client'
-            else:
-                state['remote_ip'] = ''
-                state['mode'] = 'Server'
+                state['up_since'] = get_date(date_string=parts[0], uts=True)
+                state['connected'] = parts[1]
+                state['success'] = parts[2]
+                if parts[3]:
+                    state['local_ip'] = ip_address(parts[3])
+                else:
+                    state['local_ip'] = ''
+                if parts[4]:
+                    state['remote_ip'] = ip_address(parts[4])
+                    state['mode'] = 'Client'
+                else:
+                    state['remote_ip'] = ''
+                    state['mode'] = 'Server'
+        return state
 
-    return state
+    def parse_stats(self, data):
 
-
-def openvpn_parse_stats(data):
-
-    stats = {}
-
-    line = re.sub('SUCCESS: ', '', data)
-    tmp = line.split(',')
-
-    if args.debug:
-        debug("=== begin split line\n{0!s}\n=== end split line".format(tmp))
-
-    stats['nclients'] = int(re.sub('nclients=', '', tmp[0]))
-    stats['bytesin'] = int(re.sub('bytesin=', '', tmp[1]))
-    stats['bytesout'] = int(re.sub('bytesout=', '', tmp[2]).replace('\r\n', ''))
-
-    return stats
-
-
-def openvpn_parse_status(data):
-
-    client_section = False
-    routes_section = False
-    status_version = 1
-    sessions = {}
-    client_session = {}
-    gi = GeoIP.open(args.geoip_data, GeoIP.GEOIP_STANDARD)
-
-    for line in data.splitlines():
-
-        if ',' in line:
-            tmp = line.split(',')
-        else:
-            tmp = line.split('\t')
+        stats = {}
+        line = re.sub('SUCCESS: ', '', data)
+        parts = line.split(',')
 
         if args.debug:
-            debug("=== begin split line\n{0!s}\n=== end split line".format(tmp))
+            debug("=== begin split line\n{0!s}\n=== end split line".format(parts))
 
-        if tmp[0].startswith('GLOBAL'):
-            break
-        if tmp[0] == 'HEADER':
-            status_version = 3
-            if tmp[1] == 'CLIENT_LIST':
+        stats['nclients'] = int(re.sub('nclients=', '', parts[0]))
+        stats['bytesin'] = int(re.sub('bytesin=', '', parts[1]))
+        stats['bytesout'] = int(re.sub('bytesout=', '', parts[2]).replace('\r\n', ''))
+
+        return stats
+
+    def parse_status(self, data):
+        client_section = False
+        routes_section = False
+        status_version = 1
+        sessions = {}
+        client_session = {}
+        gi = GeoIP.open(args.geoip_data, GeoIP.GEOIP_STANDARD)
+
+        for line in data.splitlines():
+
+            if ',' in line:
+                parts = line.split(',')
+            else:
+                parts = line.split('\t')
+
+            if args.debug:
+                debug("=== begin split line\n{0!s}\n=== end split line".format(parts))
+
+            if parts[0].startswith('GLOBAL'):
+                break
+            if parts[0] == 'HEADER':
+                status_version = 3
+                if parts[1] == 'CLIENT_LIST':
+                    client_section = True
+                    routes_section = False
+                if parts[1] == 'ROUTING_TABLE':
+                    client_section = False
+                    routes_section = True
+                continue
+            if parts[0] == 'Updated':
+                continue
+            if parts[0] == 'Common Name':
+                status_version = 1
                 client_section = True
                 routes_section = False
-            if tmp[1] == 'ROUTING_TABLE':
+                continue
+            if parts[0] == 'ROUTING TABLE' or parts[0] == 'Virtual Address':
+                status_version = 1
                 client_section = False
                 routes_section = True
-            continue
-        if tmp[0] == 'Updated':
-            continue
-        if tmp[0] == 'Common Name':
-            status_version = 1
-            client_section = True
-            routes_section = False
-            continue
-        if tmp[0] == 'ROUTING TABLE' or tmp[0] == 'Virtual Address':
-            status_version = 1
-            client_section = False
-            routes_section = True
-            continue
-        if tmp[0].startswith('>CLIENT'):
-            continue
+                continue
+            if parts[0].startswith('>CLIENT'):
+                continue
 
-        session = {}
-        if tmp[0] == 'TUN/TAP read bytes':
-            client_session['tuntap_read'] = int(tmp[1])
-            continue
-        if tmp[0] == 'TUN/TAP write bytes':
-            client_session['tuntap_write'] = int(tmp[1])
-            continue
-        if tmp[0] == 'TCP/UDP read bytes':
-            client_session['tcpudp_read'] = int(tmp[1])
-            continue
-        if tmp[0] == 'TCP/UDP write bytes':
-            client_session['tcpudp_write'] = int(tmp[1])
-            continue
-        if tmp[0] == 'Auth read bytes':
-            client_session['auth_read'] = int(tmp[1])
-            sessions['Client'] = client_session
-            continue
-        if client_section and not routes_section:
-            if status_version == 1:
-                ident = tmp[1]
-                sessions[ident] = session
-                session['username'] = tmp[0]
-                remote_ip, port = tmp[1].split(':')
-                session['bytes_recv'] = int(tmp[2])
-                session['bytes_sent'] = int(tmp[3])
-                session['connected_since'] = get_date(tmp[4])
-            if status_version == 3:
-                ident = tmp[2]
-                sessions[ident] = session
-                session['username'] = tmp[1]
-                if tmp[2].count(':') == 1:
-                    remote_ip, port = tmp[2].split(':')
+            session = {}
+            if parts[0] == 'TUN/TAP read bytes':
+                client_session['tuntap_read'] = int(parts[1])
+                continue
+            if parts[0] == 'TUN/TAP write bytes':
+                client_session['tuntap_write'] = int(parts[1])
+                continue
+            if parts[0] == 'TCP/UDP read bytes':
+                client_session['tcpudp_read'] = int(parts[1])
+                continue
+            if parts[0] == 'TCP/UDP write bytes':
+                client_session['tcpudp_write'] = int(parts[1])
+                continue
+            if parts[0] == 'Auth read bytes':
+                client_session['auth_read'] = int(parts[1])
+                sessions['Client'] = client_session
+                continue
+            if client_section and not routes_section:
+                if status_version == 1:
+                    ident = parts[1]
+                    sessions[ident] = session
+                    session['username'] = parts[0]
+                    remote_ip, port = parts[1].split(':')
+                    session['bytes_recv'] = int(parts[2])
+                    session['bytes_sent'] = int(parts[3])
+                    session['connected_since'] = get_date(parts[4])
+                if status_version == 3:
+                    ident = parts[2]
+                    sessions[ident] = session
+                    session['username'] = parts[1]
+                    if parts[2].count(':') == 1:
+                        remote_ip, port = parts[2].split(':')
+                    else:
+                        remote_ip = parts[2]
+                        port = None
+                    remote_ip_address = ip_address(remote_ip)
+                    local_ip = parts[3]
+                    if local_ip == '':
+                        session['local_ip'] = ''
+                    else:
+                        session['local_ip'] = ip_address(parts[3])
+                    session['bytes_recv'] = int(parts[4])
+                    session['bytes_sent'] = int(parts[5])
+                    session['connected_since'] = get_date(parts[7], uts=True)
+                session['location'] = 'Unknown'
+                if type(remote_ip_address) == IPv6Address and \
+                        remote_ip_address.ipv4_mapped is not None:
+                    session['remote_ip'] = remote_ip_address.ipv4_mapped
                 else:
-                    remote_ip = tmp[2]
-                    port = None
-                remote_ip_address = ip_address(remote_ip)
-                local_ip = tmp[3]
-                if local_ip == '':
-                    session['local_ip'] = ''
+                    session['remote_ip'] = remote_ip_address
+                if port:
+                    session['port'] = int(port)
                 else:
-                    session['local_ip'] = ip_address(tmp[3])
-                session['bytes_recv'] = int(tmp[4])
-                session['bytes_sent'] = int(tmp[5])
-                session['connected_since'] = get_date(tmp[7], uts=True)
-            session['location'] = 'Unknown'
-            if type(remote_ip_address) == IPv6Address and \
-                    remote_ip_address.ipv4_mapped is not None:
-                session['remote_ip'] = remote_ip_address.ipv4_mapped
+                    session['port'] = ''
+                if session['remote_ip'].is_private:
+                    session['location'] = 'RFC1918'
+                else:
+                    gir = gi.record_by_addr(str(session['remote_ip']))
+                    if gir is not None:
+                        session['location'] = gir['country_code']
+                        session['city'] = gir['city']
+                        session['country_name'] = gir['country_name']
+                        session['longitude'] = gir['longitude']
+                        session['latitude'] = gir['latitude']
+            if routes_section and not client_section:
+                if status_version == 1:
+                    ident = parts[2]
+                    sessions[ident]['local_ip'] = ip_address(parts[0])
+                    sessions[ident]['last_seen'] = get_date(parts[3])
+                if status_version == 3:
+                    ident = parts[3]
+                    sessions[ident]['last_seen'] = get_date(parts[5], uts=True)
+
+        if args.debug:
+            if sessions:
+                pretty_sessions = pformat(sessions)
+                debug("=== begin sessions\n{0!s}\n=== end sessions".format(pretty_sessions))
             else:
-                session['remote_ip'] = remote_ip_address
-            if port:
-                session['port'] = int(port)
-            else:
-                session['port'] = ''
-            if session['remote_ip'].is_private:
-                session['location'] = 'RFC1918'
-            else:
-                gir = gi.record_by_addr(str(session['remote_ip']))
-                if gir is not None:
-                    session['location'] = gir['country_code']
-                    session['city'] = gir['city']
-                    session['country_name'] = gir['country_name']
-                    session['longitude'] = gir['longitude']
-                    session['latitude'] = gir['latitude']
-        if routes_section and not client_section:
-            if status_version == 1:
-                ident = tmp[2]
-                sessions[ident]['local_ip'] = ip_address(tmp[0])
-                sessions[ident]['last_seen'] = get_date(tmp[3])
-            if status_version == 3:
-                ident = tmp[3]
-                sessions[ident]['last_seen'] = get_date(tmp[5], uts=True)
+                debug("no sessions")
 
-    if args.debug:
-        if sessions:
-            pretty_sessions = pformat(sessions)
-            debug("=== begin sessions\n{0!s}\n=== end sessions".format(pretty_sessions))
-        else:
-            debug("no sessions")
+        return sessions
 
-    return sessions
-
-
-def openvpn_parse_version(data):
-
-    for line in data.splitlines():
-        if line.startswith('OpenVPN'):
-            return line.replace('OpenVPN Version: ', '')
+    def parse_version(self, data):
+        for line in data.splitlines():
+            if line.startswith('OpenVPN'):
+                return line.replace('OpenVPN Version: ', '')
 
 
 def print_session_table_headers(vpn_mode):
@@ -593,21 +593,6 @@ def print_html_footer():
     print('</div></body></html>')
 
 
-def openvpn_collect_data(vpn):
-
-    version = openvpn_send_command(vpn, 'version\n')
-    vpn['version'] = openvpn_parse_version(version)
-
-    state = openvpn_send_command(vpn, 'state\n')
-    vpn['state'] = openvpn_parse_state(state)
-
-    stats = openvpn_send_command(vpn, 'load-stats\n')
-    vpn['stats'] = openvpn_parse_stats(stats)
-
-    status = openvpn_send_command(vpn, 'status 3\n')
-    vpn['sessions'] = openvpn_parse_status(status)
-
-
 def init_vars(settings):
 
     site = 'Example'
@@ -632,33 +617,27 @@ def init_vars(settings):
     return site, logo, maps, latitude, longitude
 
 
-def main():
+def print_webpage(cfg, monitor):
 
-    cfg = config_loader(args.config)
-    settings = cfg.get_settings()
-    vpns = cfg.get_vpns()
-
-    site, logo, maps, latitude, longitude = init_vars(settings)
-
-    print_html_header(site, logo, list(vpns.items()), maps)
-
-    for key, vpn in list(vpns.items()):
-        s = openvpn_connect(vpn)
-        if s:
-            openvpn_disconnect(s)
-            openvpn_collect_data(vpn)
+    site, logo, maps, latitude, longitude = init_vars(cfg.settings)
+    print_html_header(site, logo, list(monitor.vpns.items()), maps)
+    for key, vpn in list(monitor.vpns.items()):
         if vpn['socket_connected']:
             print_vpn(vpn)
         else:
             print_unavailable_vpn(vpn)
-
     if maps:
-        print_maps_html(vpns, latitude, longitude)
-
+        print_maps_html(monitor.vpns, latitude, longitude)
     print_html_footer()
 
+
+def main():
+
+    cfg = config_loader(args.config)
+    monitor = openvpn_monitor(cfg.vpns)
+    print_webpage(cfg, monitor)
     if args.debug:
-        pretty_vpns = pformat((dict(vpns)))
+        pretty_vpns = pformat((dict(monitor.vpns)))
         debug("=== begin vpns\n{0!s}\n=== end vpns".format(pretty_vpns))
 
 
