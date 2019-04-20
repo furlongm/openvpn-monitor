987266d31ae9dee1cf10fa6f9141180ece7b21e2
Marcus Furlong <furlongm@gmail.com>
2019-04-20 16:30:11 -0400

    update copyright and license blurb

--- openvpn-monitor.py
+++ openvpn-monitor.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Licensed under GPL v3
 # Copyright 2011 VPAC <http://www.vpac.org>
-# Copyright 2012-2016 Marcus Furlong <furlongm@gmail.com>
+# Copyright 2012-2019 Marcus Furlong <furlongm@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 only.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
 
 from __future__ import absolute_import
 from __future__ import division
